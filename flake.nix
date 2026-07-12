d7dcc86690608b6701abbc3e7cd25e875bdd0845
zumuvik <toovalvedota2@gmail.com>
2026-07-13 00:44:10 +0300

    fix for stable version

--- flake.nix
+++ flake.nix
@@ -2,49 +2,51 @@
   description = "Мой первый конфиг с Home Manager";
 
   inputs = {
-    # Основной репозиторий пакетов (unstable)
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
-    
-    # Правильное подключение Home Manager
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-        
-       codebase-memory-mcp = {
-      url = "github:DeusData/codebase-memory-mcp";
-      inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = inputs @ { self, nixpkgs, zen-browser, home-manager, codebase-memory-mcp, ... }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./hardware-configuration.nix
-            ./configuration.nix
-        
-        # Подключаем оверлеи, чтобы новые пакеты были доступны в pkgs
-        {
-          nixpkgs.overlays = [
-            opencode-nix.overlays.default
-            nix-vscode-extensions.overlays.default
-          ];
-        }
-        
-        # Настройка Home Manager внутри системного конфига
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.extraSpecialArgs = { inherit inputs; };
-          home-manager.users.zumuvik = { config, pkgs, ... }: {
-            imports = [ 
-              inputs.zen-browser.homeModules.default 
-              ./home.nix 
-            ];
-          };
-        }
-      ];
-    };
+    
   };
-}
 
+  outputs =
+    inputs @ {
+      self,
+      nixpkgs,
+      zen-browser,
+      home-manager,
+      ...
+    }:
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        specialArgs = {
+          inherit inputs;
+        };
+
+        modules = [
+          ./hardware-configuration.nix
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+
+          {
+            home-manager.extraSpecialArgs = {
+              inherit inputs;
+            };
+
+            home-manager.users.zumuvik = { config, pkgs, ... }: {
+              imports = [
+                inputs.zen-browser.homeModules.default
+                ./home.nix
+              ];
+            };
+          }
+        ];
+      };
+    };
+}
--- profile/core/users.nix
+++ profile/core/users.nix
@@ -6,7 +6,7 @@
      shell = pkgs.zsh;
      extraGroups = [ "networkmanager" "wheel" "video" "audio" "input" ]; # Enable ‘sudo’ for the user.
      packages = with pkgs; [
-    vscodium python3 python3Packages.pip brightnessctl ayugram-desktop gh
+    vscodium python3 python3Packages.pip brightnessctl ayugram-desktop gh codex
      ];
    };
 }

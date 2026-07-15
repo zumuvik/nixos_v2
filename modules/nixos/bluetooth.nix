cf095fba62ab7103c7fb65e8f98a864732d2c4c6
zumuvik <toovalvedota2@gmail.com>
2026-07-16 01:55:24 +0300

    add: multi host

---
+++ modules/nixos/bluetooth.nix
@@ -0,0 +1,17 @@
+{
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+
+    settings = {
+      General = {
+        Experimental = true;     
+	FastConnectable = true;
+      };
+
+      Policy = {
+        AutoEnable = true;
+      };
+    };
+   };
+  }
